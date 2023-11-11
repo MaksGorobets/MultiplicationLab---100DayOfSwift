aa8c092b4178c8116935e79ef63c0e573c495fd0
MaksGorobets <gorobets542@gmail.com>
2023-11-11 19:02:13 +0200

    Initial Commit

---
+++ MultiplicationLab/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MultiplicationLab
+//
+//  Created by Maks Winters on 11.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MultiplicationLab/MultiplicationLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationLabApp.swift
+//  MultiplicationLab
+//
+//  Created by Maks Winters on 11.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
