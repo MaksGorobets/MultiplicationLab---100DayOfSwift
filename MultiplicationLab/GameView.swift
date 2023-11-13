2241c6a5eb69545df330d94d34ec34754fbf6f68
MaksGorobets <gorobets542@gmail.com>
2023-11-13 15:50:24 +0200

    Fixed buttons, working on NavStack

---
+++ MultiplicationLab/GameView.swift
@@ -0,0 +1,18 @@
+//
+//  GameView.swift
+//  MultiplicationLab
+//
+//  Created by Maks Winters on 13.11.2023.
+//
+
+import SwiftUI
+
+struct GameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameView()
+}
